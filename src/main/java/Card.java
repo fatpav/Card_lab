c58acb94d05aeb0181d41b3a7878f1e517493b89
fatpav <fat_pav@hotmail.com>
2020-11-19 14:06:16 +0000

    first test passed

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,12 @@
 public class Card {
+    
+    private SuitType suit;
+    
+    public Card(SuitType suit){
+        this.suit = suit;
+    }
+    
+    public SuitType getSuit(){
+        return this.suit;
+    }
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+
+    Card playingCard;
+
+    @Before
+    public void before(){
+        playingCard = new Card(SuitType.HEARTS);
+    }
+
+    @Test
+    public void cardHasSuit(){
+        assertEquals(SuitType.HEARTS, playingCard.getSuit());
+    }
 }
